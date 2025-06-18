e814cb1f651a8e1273750bc132cb672c7240d8bd
SK <snehakokh@gmail.com>
2025-06-18 14:45:03 +0545

    Removed unwanted outer files and cleaned project structure

--- src/Line/Line.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Line = () => {
-  return (
-    <div className='h-20 w-[100%] bg-gray-300'></div>
-  )
-}
-
-export default Line
--- src/config/Axios.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const token = localStorage.getItem("accessToken")
-export const http = axios.create({
-    baseURL:import.meta.env.VITE_BACKEND_API,
-    headers: {
-        Authorization:token
-    }
-})
